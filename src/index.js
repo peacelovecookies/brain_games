98040189f631524692b7be057ef8903d182ec19f
Pavel Kim <peacelovecookiesstudy@gmail.com>
2020-10-20 09:15:05 +0500

    put logic in func, export default (of course, dummy, you are calling func in bin)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-
-const name = readlineSync.question('May I have your name, please?');
-
-console.log(`Nice to meet you, ${name}. So, now let's play!`);
+export default () => {
+  console.log('Welcome to the Brain Games!');
+  
+  const name = readlineSync.question('May I have your name, please? ');
+  console.log(`Nice to meet you, ${name}. So, now let's play!`);
+};
