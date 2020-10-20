87f8819d320b1e0bb68e85de7513e327a8f59576
Pavel Kim <peacelovecookiesstudy@gmail.com>
2020-10-20 10:38:22 +0500

    add information about avaliable games

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import readlineSync from 'readline-sync';
+import fs from 'fs';
+
+const games = fs.readdirSync(`${__dirname}/bin/`)
+  .map((filename) => filename.slice(0, -3))
+  .filter((game) => game !== 'brain-games');
+
+console.log(games);
 
 export default () => {
   console.log('\n Welcome to the Brain Games!\n');
 
   const name = readlineSync.question('May I have your name, please? ');
-  console.log(`Nice to meet you, ${name}. So, now let's play!`);
+  console.log(`Nice to meet you, ${name}. Now let's play!\n`);
+
+  console.log(`Pick your game:\n ${games.join('\n')}`); // TODO: add possibility of picking game right from terminal
 };
