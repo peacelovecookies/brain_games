1b7d90af4b1714c7fa4ad74367385edcbd45cde8
Pavel Kim <peacelovecookiesstudy@gmail.com>
2020-10-20 09:16:18 +0500

    add \n for a better reading

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-  console.log('Welcome to the Brain Games!');
+  console.log('\n Welcome to the Brain Games!\n');
   
   const name = readlineSync.question('May I have your name, please? ');
   console.log(`Nice to meet you, ${name}. So, now let's play!`);
