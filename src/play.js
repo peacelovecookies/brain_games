d90b6204a876521f139a36184523882abfeb8a0c
Pavel Kim <peacelovecookiesstudy@gmail.com>
2020-10-20 15:17:04 +0500

    move config to src (again, it's not a module, idieminor changes of game text)

--- src/play.js
+++ src/play.js
@@ -1,21 +1,26 @@
 import readlineSync from 'readline-sync';
-import { rounds, lives } from '../config';
+import { rounds, lives } from './config';
 
 export default (task, game) => {
   console.log(task);
   const currentLives = [...lives];
-  for (let i = 0; i < rounds; i += 1) {
-    console.log(`Your lives: ${lives.join(' ')}\n`);
+  let currentRounds = rounds;
+  for (let i = 0; i < currentRounds; i += 1) {
+    if (currentLives.length === 0) {
+      console.log('\nOh, no! You lost. Do you want to play one more time?'); // TODO: add play game again
+      return;
+    }
+
+    console.log(`\nRound ${i + 1}`);
+    console.log(`Your lives: ${currentLives.join(' ')}\n`);
 
     const { question, correctAnswer } = game();
     const answer = readlineSync.question(question);
     if (answer !== correctAnswer) {
       currentLives.pop();
-      console.log('Sorry to say, but you made a mistake. Try again...');
-    }
-    if (currentLives.length === 0) {
-      console.log('You lost. Do you want to play one more time?'); // TODO: add play game again
+      currentRounds += 1;
+      console.log('\nSorry to say, but you made a mistake. Try again...');
     }
   }
-  console.log('Congratulations, you are, smart-ass!');
+  console.log('Congratulations, you win! You are, smart-ass!');
 };
